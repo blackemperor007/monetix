c4d4a2a6e3c345e214f44381af4158eed48be4dc
wesley pagnol <emperorb007@gmail.com>
2026-01-08 14:31:58 +0100

    wrapper edit finish

--- app/components/wrapper.tsx
+++ app/components/wrapper.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 type WrapperProps = {
   children: React.ReactNode;
@@ -7,7 +8,8 @@ type WrapperProps = {
 const Wrapper = ({ children }: WrapperProps) => {
   return (
     <div>
-      <div>{children}</div>
+        {/* <Navbar /> */}
+      <div className='px-5 md:px-[10%] mt-8 mb-10'>{children}</div>
     </div>
   );
 };
