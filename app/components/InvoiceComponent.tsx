7b59a05f609a12bd3036bd72015b52b27f21b74f
wesley pagnol <emperorb007@gmail.com>
2026-01-09 02:06:51 +0100

    getstatut init

--- app/components/InvoiceComponent.tsx
+++ app/components/InvoiceComponent.tsx
@@ -70,7 +70,7 @@ const InvoiceComponent: React.FC<InvoiceComponentProps> = ({invoice,index,}) => 
   return (
     <div className="bg-base-200/90 p-5 rounded-xl space-y-2 shadow">
       <div className="flex justify-between items-center w-full">
-        {/* <div>{getStatusBadge(invoice.status)}</div> */}
+        <div>{getStatusBadge(invoice.status)}</div>
         <Link className="btn btn-accent btn-sm" href={`/invoice/${invoice.id}`}>
           Plus
           <SquareArrowOutUpRight className="w-4" />
